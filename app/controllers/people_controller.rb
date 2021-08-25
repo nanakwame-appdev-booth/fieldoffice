2f3e4763db7e64642c74e2d801f3016aee8649eb
jasonantwi <jantwi@chicagobooth.edu>
2021-08-25 20:11:13 +0000

    About to make some DB changes

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,7 @@
 class PeopleController < ApplicationController
 
   def index
-    @list_of_agents = Agent.all
+    @list_of_agents = Agent.all.order({ :first_name => :asc})
 
     render ({ :template => "people/index.html.erb"}) 
   end
