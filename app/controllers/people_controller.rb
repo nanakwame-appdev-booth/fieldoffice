417070e72c1147bebea2025860f6c07f1f87918a
jasonantwi <jantwi@chicagobooth.edu>
2021-08-26 02:15:33 +0000

    Started on backend for add person form

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -37,7 +37,30 @@ class PeopleController < ApplicationController
   end
 
   def add_person
+    #Parameters: {"fname"=>"Jason", "lname"=>"Antwi", "role"=>"Candidate", "email"=>"Jason.Antwi@gmail.com", "phone"=>"7754011975", "address1"=>"1234 Main Street", "address2"=>"386", "city"=>"Oakland", "state"=>"California", "zip"=>"94607", "individual_id"=>"add_person"}
+
+    first_name = param.fetch("fname")
+    last_name = param.fetch("lname")
+    role = param.fetch("role")
+    email = param.fetch("email")
+    phone = param.fetch("phone")
+    address1 = param.fetch("address1")
+    address2 = param.fetch("address2")
+    city = param.fetch("city")
+    state = param.fetch("state")
+    zip = param.fetch("zip")
+
+    new_individual = Individual.new 
+
+    new_individual.first_name = first_name
+    new_individual.last_name = last_name
+    new_individual.phone_number = phone
+    new_individual.email = email
+
+    new_individual.save
+
         
+
 
     render ({ :template => "people/add_individual.html.erb"})
   end
